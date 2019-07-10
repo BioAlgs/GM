5b1b623f0f6696e15205385c7c4f11d52be30b72
Xin Xing <xin.xing.ustc@gmail.com>
2019-07-10 17:58:08 -0400

    update

---
+++ R/tomato.R
@@ -0,0 +1,17 @@
+#' GWAS data set with a panel of 292 tomato accessions
+#'
+#' This  dataset  is  publicly  available at (\href{ftp://ftp.solgenomics.net/manuscripts/Bauchet_2016/})
+#'
+#'
+#' @usage data(tommato)
+#'
+#' @format each column corresponding a SNP
+#'
+#' @keywords datasets
+#'
+#' @references Bauchet et al. (2017) Theoretical and applied genetics, 130(5):875–889.
+#'
+#'
+#' @examples
+#' data(tommato)
+"tomato"
